72d9de88dc630695433d07a1615156fe6a754e5f
Tyler Corbett <thetylercorbett@gmail.com>
2020-03-13 16:19:03 -0700

    reset css

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import styled from 'styled-components';
+import './reset.css';
 
-const Test = styled.div(props => ({
-  background: props.background,
-  height: '3rem',
-  width: '3rem',
-}))
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Test background="red">
-          hello world
-        </Test>
-      </header>
+    <div>
+      hello
     </div>
   );
 }
