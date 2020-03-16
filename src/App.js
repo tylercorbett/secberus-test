887ab81beb84d9a29f1422a2e79769f98749f51c
Tyler Corbett <thetylercorbett@gmail.com>
2020-03-15 21:06:41 -0700

    static dq pge

--- src/App.js
+++ src/App.js
@@ -17,7 +17,16 @@ function App() {
       {/* <Form 
         onSubmit={handleQuoteSubmit}
       /> */}
-      <SignUp/>
+      {/* <SignUp/> */}
+      <div className="disqualified column">
+        <h2>Sorry... you aren't qualified.</h2>
+        <br/>
+        <img src="https://image.shutterstock.com/image-vector/sad-face-emoji-vector-600w-1288383397.jpg" alt="Sad face" />
+        <br />
+        <p>your credit is not high enough</p>
+        <br/>
+        <h3>Questions? Reach our customer support at: 555-5555</h3>
+      </div>
     </div>
   );
 }
