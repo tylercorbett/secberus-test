39fea99e067de3ed52aef0ba1a9282852e9408d4
Tyler Corbett <thetylercorbett@gmail.com>
2020-03-15 19:39:15 -0700

    basic flexbox layout for form

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -12,18 +12,56 @@ const Frame = styled.section(props => ({
     alignItems: 'center',
     
   },
+
+  '.info-section': {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    background: 'coral',
+    padding: '1rem',
+    margin: '1.5rem 0',
+  },
 }))
 
 const Form = () => {
 
   return (
     <Frame>
-      <div className="column">
-        <FormComponents.Label>
-          Auto Purchase Price
-        </FormComponents.Label>
-        <FormComponents.Input type="number"/>
-      </div>
+      <section className="info-section">
+        <div className="column">
+          <FormComponents.Label>
+            Auto Purchase Price
+          </FormComponents.Label>
+          <FormComponents.Input type="number"/>
+        </div>
+        <div className="column">
+          <FormComponents.Label>
+            Auto Make
+          </FormComponents.Label>
+          <FormComponents.Input type="text"/>
+        </div>
+        <div className="column">
+          <FormComponents.Label>
+            Auto Model
+          </FormComponents.Label>
+          <FormComponents.Input type="text"/>
+        </div>
+      </section>
+
+      <section className="info-section">
+        <div className="column">
+          <FormComponents.Label>
+            Estimated Yearly Income
+          </FormComponents.Label>
+          <FormComponents.Input type="number"/>
+        </div>
+        <div className="column">
+          <FormComponents.Label>
+            Estimated Credit Score
+          </FormComponents.Label>
+          <FormComponents.Input type="number"/>
+        </div>
+      </section>
     </Frame>
   );
 };
