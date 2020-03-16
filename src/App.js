91fa510df78f684bef452ae6258105cc7f2a3490
Tyler Corbett <thetylercorbett@gmail.com>
2020-03-15 21:13:46 -0700

    page flows by state

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import SignUp from './components/sign-up/SignUp';
 
 
 function App() {
-  const [isDisqualified, setIsDisqualified] = useState(true);
+  const [isDisqualified, setIsDisqualified] = useState(false);
+  const [isQuoteSubmitted, setIsQuoteSubmitted] = useState(false);
 
   const handleQuoteSubmit = formData => {
     console.log(formData);
@@ -15,19 +16,23 @@ function App() {
   return (
     <div className="App">
       <header>Tyler's Auto Loans</header>
-      {/* <Form 
-        onSubmit={handleQuoteSubmit}
-      /> */}
-      {/* <SignUp/> */}
-      {isDisqualified && <div className="disqualified column">
-        <h2>Sorry... you aren't qualified.</h2>
-        <br/>
-        <img src="https://image.shutterstock.com/image-vector/sad-face-emoji-vector-600w-1288383397.jpg" alt="Sad face" />
-        <br />
-        <p>your credit is not high enough</p>
-        <br/>
-        <h3>Questions? Reach our customer support at: 555-5555</h3>
-      </div>}
+      {
+        !isQuoteSubmitted 
+          ? <Form 
+              onSubmit={handleQuoteSubmit}
+            />
+          : isDisqualified 
+            ? <div className="disqualified column">
+                <h2>Sorry... you aren't qualified.</h2>
+                <br/>
+                <img src="https://image.shutterstock.com/image-vector/sad-face-emoji-vector-600w-1288383397.jpg" alt="Sad face" />
+                <br />
+                <p>your credit is not high enough</p>
+                <br/>
+                <h3>Questions? Reach our customer support at: 555-5555</h3>
+              </div>
+            : <SignUp/>
+        }
     </div>
   );
 }
